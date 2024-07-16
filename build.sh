7d9be18cd535e48ae928eb8f0271c5ba94ac98bc
hanzy1110 <hanzy1110@github.com>
2024-07-16 10:49:27 -0300

    no superuser

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ mkdir -p static
 
 # Convert static asset files
 python manage.py collectstatic --no-input
-
 # Apply any outstanding database migrations
 python manage.py migrate
+python manage.py createsuperuser --no-input
