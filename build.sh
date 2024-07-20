bb4e5045b2088091385f06c49b9a461277916d85
hanzy1110 <hanzy1110@github.com>
2024-07-20 11:29:14 -0300

    maybe this

--- build.sh
+++ build.sh
@@ -11,4 +11,4 @@ mkdir -p static
 python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
-python manage.py createsuperuser --no-input
+# python manage.py createsuperuser --no-input
